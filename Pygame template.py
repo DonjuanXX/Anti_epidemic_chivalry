7e42bee88b57547e8378a55db15ab67943f6cb09
DonjuanXX <minghan.idward@gmail.com>
2020-12-01 22:31:12 +0000

    first commit

---
+++ Pygame template.py
@@ -0,0 +1,36 @@
+import pygame
+import random
+
+WIDTH = 300
+HEIGHT = 400
+FPS = 30  # screen update
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()
+pygame.mixer.init()  # music
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+# Game loop
+running = True
+while running:
+    # keep loop running at the right speed
+    clock.tick(FPS)
+    # Process input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # Update
+
+    # Draw / render
+    screen.fill(BLACK)
+    # after drawing , flip display
+    pygame.display.flip()
+
+pygame.quit()
