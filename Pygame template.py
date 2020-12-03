9a4be50d8aff4bad208aa70a622a9e22a57c9e80
DonjuanXX <minghan.idward@gmail.com>
2020-12-03 22:00:09 +0800

    class 2

--- Pygame template.py
+++ Pygame template.py
@@ -1,5 +1,7 @@
 import pygame
+import os
 
+# opengameart.org
 WIDTH = 800
 HEIGHT = 600
 FPS = 30  # screen update
@@ -10,18 +12,32 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+# set up assets folders
+# Win:"C:\xx\"
+# Linux: "/USER/XXX"
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
 
 class Player(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill(GREEN)
+        self.image = pygame.image.load(
+            os.path.join(img_folder, "BODY_male.png")).convert()
+        # convert函数作用是将bai图片转化为duSurface对象，pygame现在会自动这么做
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.y_speed = 5
 
     def update(self):
         self.rect.x += 5
+        self.rect.y += self.y_speed
+        if self.rect.bottom > HEIGHT - 200:
+            self.y_speed = -5
+        if self.rect.top < 200:
+            self.y_speed = 5
         if self.rect.left > WIDTH:
             self.rect.right = 0
 
@@ -48,7 +64,7 @@ while running:
     all_sprites.update()
 
     # Draw / render
-    screen.fill(BLACK)
+    screen.fill(BLUE)
     all_sprites.draw(screen)
     # after drawing , flip display
     pygame.display.flip()
