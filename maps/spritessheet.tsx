aadc54b51455c7e2d72432a87c9bbdae13f159c9
DonjuanXX <minghan.idward@gmail.com>
2020-12-09 10:17:03 +0000

    add source

---
+++ maps/spritessheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.11.25" name="spritessheet" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+ <image source="spritessheet.png" trans="000000" width="256" height="256"/>
+</tileset>
